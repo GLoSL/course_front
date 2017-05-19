c76cf1bfe19278bd674905dea442f2c68d0279a3
GLoSL <felix.og@laposte.net>
2017-05-19 10:06:41 +0200

    added a button component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 //TOUT CE QUI EST ENTRE {} DANS REACT CORRESPOND A DU CODE JS
 
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; //importe Component de base de react et react
+import logo from './logo.svg'; // définition de la variable logo
 import './App.css';
+import Button from './components/button' ; // import du component Button
+import {textButton} from './test.js'; // import de la variable textButton
 
-import Button from './components/button' ;
-import {textButton} from './test.js';
+class App extends Component { //création d'une classe App dérivée de Component
 
-class App extends Component {
+  majState = () => {
+    console.log ("Je suis là !!!");
+  }
   render() {
     return (
-      <div className="App">
+      <div className="App">{/* className est équivalent à class, utilisé ici car on mélange html et js (et class existe déjà en js ex: ligne 5); commentaire en js entre accolades */}
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2></h2>
+          <img src={logo} className="App-logo" alt="logo" /> {/*{logo}: insertion de la variable logo importée plus haut*/}
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Button text={textButton} onClick={this.majState}/>
+        <Button text={textButton} onClick={this.majState}/>{/*on affiche le component Button dans le DOM virtuel, on lui assigne une props text qui a comme valeur la varibale textButton*/}
+        {/*quand on clique sur le bouton la fonction majState est appelée*/}
       </div>
     );
   }
--- src/components/button.js
+++ src/components/button.js
@@ -1,15 +1,24 @@
-import React, {Component} from 'react';
+//CREATION D'UN COMPONENT
+//
 
+import React, {Component} from 'react';
 
 class Button extends Component {
 
   state = {
     text: this.props.text
   }
+  /*le state permet de stocker les données de la classe, on l'utilise en interne tandis que les props peuvent venir d'un fichier interne*/
 
   componentWillReceiveProps (nextProps) {
     this.setState({text: nextProps.text})
   }
+
+
+  editText = () => {
+    return "je rajoute ça" + this.state.text;
+  }
+
 
   leClick = (event) => {
     console.log("Ce que contient le bouton : " + event.target.innerText);
